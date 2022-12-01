7a4afd46b20217d038360ef96263eba3df876afa
Milton <miltonmjcp@gmail.com>
2022-12-01 14:37:27 -0300

    Security usuario

--- src/main/java/com/training/libraryapi/config/SpringConfiguration.java
+++ src/main/java/com/training/libraryapi/config/SpringConfiguration.java
@@ -1,5 +1,6 @@
 package com.training.libraryapi.config;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,26 +8,41 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Log4j2
 public class SpringConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
+    /*@Bean
     public static BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
+    }*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().authenticated().and().formLogin();
+        http.csrf().disable().authorizeHttpRequests()
+                //Gera o token para habilitação
+                //http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and().authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                //habilita formulários de login and logout
+                .formLogin()
+                .and()
+                .httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password encoded {}", passwordEncoder.encode("test"));
         auth.inMemoryAuthentication()
                 .withUser("user")
-                .password(passwordEncoder().encode("password"))
-                .authorities("USER");
+                .password(passwordEncoder.encode("password"))
+                .roles("USER");
     }
 }
